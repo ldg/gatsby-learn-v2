d764c3750d2400cb7b9751088f76eb1c41a0c87a
smldgweb <sales.ldgwebdesign.com>
2021-07-13 16:38:58 -0400

    back to square one

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+	return(
+		<Layout pageTitle="About Page">
+			<p>More Gatsby magic in the About page!</p>
+		</Layout>
+		)
+}
+
+export default AboutPage
