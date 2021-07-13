5fe8d78729bd8c998e8806bfe931c45f91973684
smldgweb <sales.ldgwebdesign.com>
2021-07-13 17:10:10 -0400

    Finished 3rd lesson

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this with maaagic :o</p>
+      <StaticImage
+      alt="Canadian Lynx"
+      src="../images/lynx-in-snow.png" />
     </Layout>
   )
 }
