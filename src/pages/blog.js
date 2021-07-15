04c4f0708ddedf0484e7df38ffea6bc23a9c138d
smldgweb <sales.ldgwebdesign.com>
2021-07-14 22:34:49 -0400

    Finished Lesson 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({data}) => {
@@ -7,25 +8,34 @@ const BlogPage = ({data}) => {
 		<Layout pageTitle="My Blog Posts">
 			<ul>
 				{
-					data.allFile.nodes.map(node => (
-						<li key={node.name}>
-							{node.name}
-						</li>) 
-					)
-				}
+			        data.allMdx.nodes.map((node) => (
+			          <article key={node.id}>
+			            <h2>{node.frontmatter.title}</h2>
+			            <p>Posted: {node.frontmatter.date}</p>
+			            <MDXRenderer>
+                          {node.body}
+                        </MDXRenderer>
+			          </article>
+			        ) )
+			      }
 			</ul>
 		</Layout>
 		)
 }
 
-export const query = graphql `
-	query {
-		allFile {
-			nodes {
-				name
-			}
-		}
-	} 
+export const query = graphql`
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
+      }
+    }
+  }
 `
 
 export default BlogPage 
